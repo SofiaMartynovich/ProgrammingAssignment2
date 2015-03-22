3c24841b6796ca15d956e3f035aff45205c41589
SofiaMartynovich <impedimenta_268@hotmail.com>
2015-03-22 19:19:24 +0300

    My commited code

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,38 @@
-## Put comments here that give an overall description of what your
-## functions do
+## Those functions are creating mechanism of inversion for martix that are
+## invertible in general and caching inverted matrix
 
-## Write a short comment describing this function
+
+## This  function takes a martix as x and creates sequence of operarions 
+## to inverse it.  There are such operations  as settting  entire matrix
+## and inverted matrix which use arguments assigned not only for the
+## environment of this particular function. Function returns list 
+## of operations
 
 makeCacheMatrix <- function(x = matrix()) {
-
+ inv <- NULL
+ set <- function (y) {
+ x <<- y 
+ inv <<- NULL
+ }
+ get <- function () x
+ setinv <- function (solve) inv <<- solve
+ getinv <- function () inv
+ list (set = set, get = get, setinv = setinv, getinv = getinv)
 }
 
 
-## Write a short comment describing this function
-
+## This  function takes makeCacheMatrix  as an argument, checks if x
+## was already defined and gets inverted martix from that environment. 
+## if x wasn't defined then it creates data in this environment and inverse it.
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+inv <- x$getinv()
+if(!is.null(inv)) {
+message("getting cached data")
+return(inv)
+}
+data <- x$get()
+inv <- solve(data, ...)
+x$setinv(inv)
+inv
+## Return a matrix that is the inverse of 'x'
 }
